a24df9722ab28724e29d87fc1a15e6fb1878c67e
Romulo Mont Serrat <romulomontserrat@gmail.com>
2015-07-24 18:39:37 -0300

    boas praticas

--- js/vip.js
+++ js/vip.js
@@ -140,18 +140,12 @@
 
 
 	dateDifference: function dateDifference(date1, date2) {
-		// The number of milliseconds in one day
-		var ONE_DAY = 1000 * 60 * 60 * 24
+		var oneDay = 1000 * 60 * 60 * 24,
+				date1Ms = date1.getTime(),
+				date2Ms = date2.getTime(),
+				differenceMs = Math.abs(date1Ms - date2Ms);
 
-		// Convert both dates to milliseconds
-		var date1_ms = date1.getTime()
-		var date2_ms = date2.getTime()
-
-		// Calculate the difference in milliseconds
-		var difference_ms = Math.abs(date1_ms - date2_ms)
-
-		// Convert back to days and return
-		return Math.round(difference_ms/ONE_DAY)
+		return Math.round(differenceMs/oneDay)
 	},
 
 	checkDate: function checkDate(date1, date2, diff) {
