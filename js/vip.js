fb0b1e1c0bb9e5584221cd1e0c26f2bafbbfc998
rmontserrat <romulomontserrat@gmail.com>
2016-06-11 11:57:51 -0300

    refatorando..

--- js/vip.js
+++ js/vip.js
@@ -1,4 +1,6 @@
-	var vip = {
+'use strict';
+
+var vip = {
 
 	/**
 	 * Number spaces of a word
@@ -6,11 +8,13 @@
 	*/
 	numberSpaces: function numberSpaces(string) {
 		var count = 0;
+
 		for(var i = 0; i < string.length; i++) {
 			if(string.charAt(i) === ' ')  {
-				count++; 
+				count++;
 			}
-		}  
+		}
+
 		return count;
 	},
 
@@ -29,7 +33,7 @@
 	 * transforms the global variable 'invalid' false and returns error message
 	 * @return {void}
 	*/
-	errorMessage: function errorMessage(msg) {
+	errorMessage: function errorMessage() {
 		invalid = true;
 	},
 
@@ -38,17 +42,19 @@
 	 * @return {string}
 	*/
 	removeaccent: function removeaccent(strToReplace) {
-	    str_accent = "áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ";
-	    str_no_accent = "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC";
-	    var newWord = "";
-	    for (var i = 0; i < strToReplace.length; i++) {
-	        if (str_accent.indexOf(strToReplace.charAt(i)) != -1) {
-	            newWord += str_no_accent.substr(str_accent.search(strToReplace.substr(i, 1)), 1);
-	        } else {
-	            newWord += strToReplace.substr(i, 1);
-	        }
-	    }
-	    return newWord;
+		var str_accent = "áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÖÔÚÙÛÜÇ",
+			str_no_accent = "aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC",
+			newWord = "";
+
+		for (var i = 0; i < strToReplace.length; i++) {
+			if (str_accent.indexOf(strToReplace.charAt(i)) != -1) {
+				newWord += str_no_accent.substr(str_accent.search(strToReplace.substr(i, 1)), 1);
+			} else {
+				newWord += strToReplace.substr(i, 1);
+			}
+		}
+
+		return newWord;
 	},
 
 	/**
@@ -58,21 +64,24 @@
 	specialCaracteres: function specialCaracteres(text) {
 		if(text.length > 0) {
 			var length = text.length-1,
-		    	caracteres = ['-', '#', ')', '(', '>', '~', '+', '*', '"', '\\'];
+				caracteres = ['-', '#', ')', '(', '>', '~', '+', '*', '"', '\\'];
 
 			caracteres.forEach(function(caracter) {
 				var newText = text;
+
 				for(var i = 0; i <= length; i++) {
 					if(text !== undefined) {
-		        		if(text[i] === caracter || parseInt(text[i])) {
-		        			newText = text.slice(i+1);
-	        			}
-	        		}
-	    		}
-	    		text = newText;
+						if(text[i] === caracter || parseInt(text[i])) {
+							newText = text.slice(i+1);
+						}
+					}
+				}
+
+				text = newText;
 			});
-	    	return text;
-	    }
+
+			return text;
+		}
 	},
 
 	/**
@@ -80,15 +89,17 @@
 	 * @return {void}
 	*/
 	titleize: function titleize(text) {
-	    var words = text.toLowerCase().split(" ");
-	    for (var a = 0; a < words.length; a++) {
+		var words = text.toLowerCase().split(" ");
+
+		for (var a = 0; a < words.length; a++) {
 			words[a] = this.specialCaracteres(this.removeaccent(words[a]));
-	        var w = words[a] || [];
-	        if(w[0]) {
-		        words[a] = w[0].toUpperCase() + w.slice(1);
-	        }
-	    }
-	    return words.join(" ");
+			var w = words[a] || [];
+			if(w[0]) {
+				words[a] = w[0].toUpperCase() + w.slice(1);
+			}
+		}
+
+		return words.join(" ");
 	},
 
 	/**
@@ -96,31 +107,36 @@
 	 * @return {array}
 	*/
 	filterMessages: function filterMessages(response, eventLong) {
-		var self = this,
+		var that = this,
 			nameList = [];
+
 		response.data.forEach(function(data) {
 			if(eventLong) {
 				var today = new Date(),
 				postDate = new Date(data.updated_time);
-				if(self.checkDate(today, postDate, 7)) {
+
+				if(that.checkDate(today, postDate, 7)) {
 					return;
 				}
 			}
 			if(data.message !== undefined && data.type === "status") {
-	        	var split;
-	        	if(data.message.indexOf('\n') > -1 && (data.message.indexOf('\n') <= 30 || data.message.indexOf('↵') <= 30)) {
-	        		split = data.message.split('\n');
-	        	} else {
-	        		split = data.message.split(',');
-	        	}
-        		split.forEach(function(word) {
-					if(self.numberSpaces(word) > 0 && self.numberSpaces(word) <= 4 && word.length <= 30) {
-        				word = self.titleize(word);
-				        nameList = nameList.concat(word.trim());
-        			}
-        		});
-    		}
+				var split;
+
+				if(data.message.indexOf('\n') > -1 && (data.message.indexOf('\n') <= 30 || data.message.indexOf('↵') <= 30)) {
+					split = data.message.split('\n');
+				} else {
+					split = data.message.split(',');
+				}
+
+				split.forEach(function(word) {
+					if(that.numberSpaces(word) > 0 && that.numberSpaces(word) <= 4 && word.length <= 30) {
+						word = that.titleize(word);
+						nameList = nameList.concat(word.trim());
+					}
+				});
+			}
 		});
+
 		return nameList.sort();
 	},
 
@@ -129,13 +145,13 @@
 	 * @return {string}
 	*/
 	getAccessToken: function getAccessToken() {
-    	var url = "https://graph.facebook.com/v2.3/oauth/access_token?client_id=1553906604861504&client_secret=937e3f0fb78250c0cd69634e84f7d21f&grant_type=client_credentials&method=get&pretty=0&sdk=joey",
-	    	xmlHttp = new XMLHttpRequest();
+		var url = "https://graph.facebook.com/v2.3/oauth/access_token?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&grant_type=client_credentials&method=get&pretty=0&sdk=joey",
+			xmlHttp = new XMLHttpRequest();
 
-	    xmlHttp.open("GET", url, false);
-	    xmlHttp.send(null);
+		xmlHttp.open("GET", url, false);
+		xmlHttp.send(null);
 
-	    return JSON.parse(xmlHttp.responseText).access_token;
+		return JSON.parse(xmlHttp.responseText).access_token;
 	},
 
 
@@ -165,23 +181,26 @@
 	 * get the event posts
 	 * @return {void}
 	*/
-    list: function list(event) {
-		var self = this,
-			access_token = self.getAccessToken(),
+	list: function list(event) {
+		var that = this,
+			access_token = that.getAccessToken(),
 			id = this.eventId(event);
-        if(!id) {
-        	this.errorMessage();
-        	return;
-        }
+
+		if(!id) {
+			this.errorMessage();
+			return;
+		}
+
 		FB.api(
-	    "/"+id, {
+		"/"+id, {
 			access_token: access_token
-	    },
-	    function (response) {
+		},
+		function (response) {
 				if (response && !response.error) {
-					var eventName = self.replaceAll(response.name, ' ', '-'),
-							start = new Date(response.start_time),
-							end = new Date(response.end_time);
+					var eventName = that.replaceAll(response.name, ' ', '-'),
+						start = new Date(response.start_time),
+						end = new Date(response.end_time);
+
 					FB.api(
 						id+"/feed", {
 							access_token: access_token,
@@ -190,15 +209,15 @@
 						},
 						function (data) {
 							if (data && !data.error) {
-						      var nameList = self.filterMessages(data, self.checkDate(end, start, 15));
-									jsPDFEditor.init(nameList, eventName);
+							  	var list = that.filterMessages(data, that.checkDate(end, start, 15));
+								jsPDFEditor.init(list, eventName);
 							} else {
-								self.errorMessage();
+								that.errorMessage();
 							}
 						}
 					);
 				} else {
-					self.errorMessage();
+					that.errorMessage();
 				}
 			}
 		)
@@ -208,12 +227,12 @@
 	 * Starts functions
 	 * @return {void}
 	*/
-    init: function init() {
-    	var self = this;
-    	document.body.querySelector('.button-submit').onclick = function(e) {
-    		e.preventDefault();
-    		var event = document.body.querySelector('#event').value;
-    		self.list(event);
-    	};
-    }
+	init: function init() {
+		var that = this;
+		document.body.querySelector('.button-submit').onclick = function(e) {
+			e.preventDefault();
+			var event = document.getElementById('event').value;
+			that.list(event);
+		};
+	}
 }
